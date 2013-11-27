f3a162b5fb824ea9d688b6f0363c1d9e6d67f609
Rutul Dave <rutul98@yahoo.com>
2013-11-26 20:20:32 -0800

    a simple observable counter with reset

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,4 @@
+class ContentsController < ApplicationController
+  def home
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KnockoutExample::Application.config.session_store :cookie_store, key: '_knockout_example_session'
